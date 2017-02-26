f79cd2b2097f686c429991b5a608c2e0dc9e3732
David Maseda <david.maseda@hotmail.com>
2017-02-26 00:32:48 +0000

    Finished

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 var express = require('express');
 var app = express();
+var moment = require('moment');
+var path = require('path');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  var filename = path.join(__dirname,"views/index.html");
+  res.sendFile(filename,function(err){
+    if(err){
+      console.log(err);
+      res.status(err.status).end();
+    }
+})});
+
+
+
+app.get('/:datestring', function(req,res) {
+  var myDate;
+  if(/^\d{8,}$/.test(req.params.datestring)) {
+    myDate = moment(req.params.datestring, "X");
+  } else {
+    myDate = moment(req.params.datestring, "MMMM D, YYYY");
+  }
+
+  if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+
+
 });
 
 // use port 3000 unless there exists a preconfigured port	
